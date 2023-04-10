f73a37b050d7142925a407a20feeacb0061f66fe
Hristo Vuchev <hristo.vuchev@egt-digital.com>
2023-04-10 11:57:37 +0300

    harpoon

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  harpoon = {
+    loaded = true,
+    path = "C:\\Users\\hristo.vuchev\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
+  },
   ["nvim-treesitter"] = {
     loaded = true,
     path = "C:\\Users\\hristo.vuchev\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
