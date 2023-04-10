746fa50b85f202fa2b9d49298ebce418e550aa38
Hristo Vuchev <hristo.vuchev@egt-digital.com>
2023-04-10 10:19:54 +0300

    clear for new config

--- lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local colorscheme = "ayu-dark"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
-
