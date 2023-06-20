bba6c86617837a38eae3baa0b30c93651965a17b
Hristo Vuchev <hristo.vuchev@egt-digital.com>
2023-06-20 17:33:20 +0300

    schange config to use 2 spaces for tab instead of tabs

--- lua/ybtre/set.lua
+++ lua/ybtre/set.lua
@@ -1,12 +1,13 @@
 --vim.opt.guicursor = ""
 vim.opt.guicursor=block
 
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
