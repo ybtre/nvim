c516919ee6281e295cc47e239e95850d63f5ad32
HVuchev <vuchev7892@gmail.com>
2023-07-07 16:09:51 +0300

    lsp remappings + softwrap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,28 +8,31 @@ lsp.ensure_installed({
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+--lsp.configure('lua-language-server', {
+--    settings = {
+--        Lua = {
+--            diagnostics = {
+--               globals = { 'vim' }
+--            }
+--        }
+--    }
+--})
 
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<A-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<A-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<A-y>'] = cmp.mapping.confirm({ select = true }),
+--  ['<A-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--  ['<A-n>'] = cmp.mapping.select_next_item(cmp_select),
+--  ['<A-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<cr>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+--cmp_mappings['<Tab>'] = nil
+--cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
@@ -58,6 +61,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<A-s>", function() vim.lsp.buf.signature_help() end, opts)
+
+  lsp.buffer_autoformat()
 end)
 
 
