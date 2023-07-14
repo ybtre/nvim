4e9df3aeabb691b2f2381590577ae4db80d1d9f6
HVuchev <vuchev7892@gmail.com>
2023-07-14 14:53:16 +0300

    learned to call vim commands in keymaps.lua (TODO/NOTE highlight)

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -35,4 +35,33 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
---:hi CursorLine gui=underline cterm=underline
+--NOTE: enable cursorline underline style on buffer enter automatically
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = function()
+    vim.cmd([[highlight CursorLine gui=underline cterm=underline]])
+  end,
+})
+
+--NOTE: enable NOTE: tags highlighting
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = function()
+    vim.cmd([[highlight Note cterm=underline ctermbg=green gui=underline guibg=none guifg=green]])
+    vim.cmd([[call matchadd("Note", "NOTE:")]])
+  end,
+})
+
+--NOTE: enable TODO: tags highlighting
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = function()
+    vim.cmd([[highlight Todo cterm=underline ctermbg=red gui=underline guibg=none guifg=red]])
+    vim.cmd([[call matchadd("Todo", "TODO:")]])
+  end,
+})
+
+--NOTE: enable IMPORTANT: tags highlighting
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = function()
+    vim.cmd([[highlight Important cterm=underline ctermbg=yellow gui=underline guibg=none guifg=yellow]])
+    vim.cmd([[call matchadd("Important", "IMPORTANT:")]])
+  end,
+})
