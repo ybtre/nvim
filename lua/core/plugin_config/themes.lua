cb20678dab91561d753be1f81ae4468b5164f32b
ybtre <vuchev7892@gmail.com>
2023-08-31 14:09:40 +0300

    theme update again

--- lua/core/plugin_config/themes.lua
+++ lua/core/plugin_config/themes.lua
@@ -1,10 +1,11 @@
 vim.o.termguicolors = true
 --vim.cmd [[ colorscheme gruvbox ]]
+--vim.cmd [[ colorscheme gruvbox8 ]]
+--vim.cmd [[ colorscheme gruvbox-baby ]]
 --vim.cmd [[ colorscheme moonbow ]]
 --vim.cmd [[ colorscheme ayu ]]
 --vim.cmd [[ colorscheme kanagawa ]]
 --vim.cmd [[ colorscheme tokyonight-night ]]
-vim.cmd [[ colorscheme gruvbox-baby ]]
 --vim.cmd [[ colorscheme quiet ]]
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
