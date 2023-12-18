c74e9089169c1d1afa16cb6625944496b3e971ca
ybtre <vuchev7892@gmail.com>
2023-12-18 16:29:59 +0200

    copilot initial setup

--- lua/core/plugin_config/lsp_zero.lua
+++ lua/core/plugin_config/lsp_zero.lua
@@ -1,57 +1,57 @@
-local lsp = require('lsp-zero')
+-- local lsp = require('lsp-zero')
 
-lsp.preset("recommended")
+-- lsp.preset("recommended")
 
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  --  ['<A-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  --  ['<A-n>'] = cmp.mapping.select_next_item(cmp_select),
-  --  ['<A-y>'] = cmp.mapping.confirm({ select = true }),
-  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<cr>'] = cmp.mapping.confirm({ select = true }),
-  -- ["<C-Space>"] = cmp.mapping.complete(),
-})
+-- local cmp = require('cmp')
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--   --  ['<A-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--   --  ['<A-n>'] = cmp.mapping.select_next_item(cmp_select),
+--   --  ['<A-y>'] = cmp.mapping.confirm({ select = true }),
+--   ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+--   ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+--   ['<cr>'] = cmp.mapping.confirm({ select = true }),
+--   -- ["<C-Space>"] = cmp.mapping.complete(),
+-- })
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
+-- lsp.setup_nvim_cmp({
+--   mapping = cmp_mappings
+-- })
 
-lsp.set_preferences({
-  suggest_lsp_servers = true,
-  sign_icons = {
-    error = 'E',
-    warn = 'W',
-    hint = 'H',
-    info = 'I'
-  }
-})
+-- lsp.set_preferences({
+--   suggest_lsp_servers = true,
+--   sign_icons = {
+--     error = 'E',
+--     warn = 'W',
+--     hint = 'H',
+--     info = 'I'
+--   }
+-- })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+-- lsp.on_attach(function(client, bufnr)
+--   local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-  vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+--   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--   vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+--   vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end, opts)
+--   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+--   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+--   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--   vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+--   vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+--   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
 
-  vim.keymap.set("n", "<leader>s", require('telescope.builtin').lsp_document_symbols, opts)
-  vim.keymap.set("n", "<leader>S", require('telescope.builtin').lsp_dynamic_workspace_symbols, opts)
+--   vim.keymap.set("n", "<leader>s", require('telescope.builtin').lsp_document_symbols, opts)
+--   vim.keymap.set("n", "<leader>S", require('telescope.builtin').lsp_dynamic_workspace_symbols, opts)
 
-  --lsp.buffer_autoformat()
-end)
+--   --lsp.buffer_autoformat()
+-- end)
 
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-require('lspconfig').ols.setup({});
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+-- require('lspconfig').ols.setup({});
 
-vim.diagnostic.config({
-  virtual_text = true
-})
+-- vim.diagnostic.config({
+--   virtual_text = true
+-- })
 
-lsp.setup()
+-- lsp.setup()
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -27,6 +27,9 @@ local plugins = {
   },
 
   -- LSP and language related
+  -- Copilot
+  "zbirenbaum/copilot.lua",
+  --"zbirenbaum/copilot-cmp",
   'ray-x/lsp_signature.nvim',
   'nvim-tree/nvim-tree.lua',
   'nvim-tree/nvim-web-devicons',
@@ -40,26 +43,27 @@ local plugins = {
     "nvim-treesitter/nvim-treesitter", 
     build = ":TSUpdate"
   },
-  {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v2.x',
-      dependencies = {
+  ---
+  --{
+      --'VonHeikemen/lsp-zero.nvim',
+      --branch = 'v2.x',
+      --dependencies = {
           -- lsp support
-          { 'neovim/nvim-lspconfig' },
-          {
-              'williamboman/mason.nvim',
-              build = function()
-                  pcall(vim.cmd, 'MasonUpdate')
-              end,
-          },
-          { 'williamboman/mason-lspconfig.nvim' },
+          --{ 'neovim/nvim-lspconfig' },
+          --{
+              --'williamboman/mason.nvim',
+              --build = function()
+                  --pcall(vim.cmd, 'MasonUpdate')
+              --end,
+          --},
+          --{ 'williamboman/mason-lspconfig.nvim' },
 
           --Autocomplete
-          { 'hrsh7th/nvim-cmp' },
-          { 'hrsh7th/cmp-nvim-lsp' },
-          { 'L3MON4D3/LuaSnip' },
-      }
-  },
+          --{ 'hrsh7th/nvim-cmp' },
+          --{ 'hrsh7th/cmp-nvim-lsp' },
+          --{ 'L3MON4D3/LuaSnip' },
+      --}
+  --},
 
   -- Language related
   'Tetralux/odin.vim',
@@ -143,3 +147,49 @@ require("solarized-osaka").setup({
   ---@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
 }) 
+
+require('copilot').setup({
+  panel = {
+    enabled = false,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<Tab>",
+      accept_word = false,
+      accept_line = false,
+      next = "<A-]>",
+      prev = "<A-[>",
+      dismiss = "<A-q>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["*"] = true,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 18.x
+  server_opts_overrides = {},
+})
+
+-- require("copilot_cmp").setup()
