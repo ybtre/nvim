68a89c968d76a6d2bbd7b8918f6ad21612b727bb
hristo.vuchev <hristo.vuchev@egt-digital.com>
2024-02-05 17:32:12 +0200

    theme update + flash nvim

--- lua/core/plugin_config/themes.lua
+++ lua/core/plugin_config/themes.lua
@@ -1,16 +1,15 @@
 vim.o.termguicolors = true
 
 --vim.cmd [[ colorscheme gruvbox8_hard ]]
-vim.cmd [[ colorscheme iceberg ]]
---vim.cmd [[ colorscheme despacio ]]
---vim.cmd [[ colorscheme moonbow ]]
+--vim.cmd [[ colorscheme iceberg ]]
 --vim.cmd [[ colorscheme ayu ]]
---vim.cmd [[ colorscheme kanagawa ]]
 --vim.cmd [[ colorscheme tokyonight-night ]]
 --vim.cmd [[ colorscheme quiet ]]
+--vim.cmd [[ colorscheme kanagawa ]]
+vim.cmd [[ colorscheme alabaster ]]
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 
 function Set_transparent_bg()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -18,13 +18,7 @@ local plugins = {
   'cocopon/iceberg.vim',
   'rebelot/kanagawa.nvim',
   'folke/tokyonight.nvim',
-  'AlessandroYorba/Despacio',
-  {
-   "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+  'p00f/alabaster.nvim',
 
   -- LSP and language related
   'ray-x/lsp_signature.nvim',
@@ -101,7 +95,24 @@ local plugins = {
         -- refer to the configuration section below
     }
   },
-
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    },
+    {
+    'stevearc/dressing.nvim',
+    opts = {},
+    }
 }
 
 local opts = {}
@@ -109,37 +120,3 @@ local opts = {}
 
 -- PLUGINS SETUPS?
 require("lazy").setup(plugins, opts)
-
-require("solarized-osaka").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
-  },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-}) 
